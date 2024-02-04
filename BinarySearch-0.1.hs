b4dfb7e201d0ab450374701e92e2655efe4fb442
MinShein <minthutakyae@gmail.com>
2024-02-04 19:32:25 +0630

    Binary Search Algorithm

--- BinarySearch-0.1.hs
+++ BinarySearch-0.1.hs
@@ -1,25 +1,21 @@
--- Created by Min Thuta Shein
--- Binary Search Algorithm
--- 2/4/2024
-
 binarySearch :: [Int] -> Int -> Int
 binarySearch [] _ = -1
-binarySearch list target = binarySearchHelper list target 0 (length list - 1) -- higher is (length list - 1) because index of the list is always less than the cardinality of the list eg. index of the list is 9 and cardinality of the list is 10
+binarySearch list target = binarySearchHelper list target 0 (length list - 1)
 
 binarySearchHelper :: [Int] -> Int -> Int -> Int -> Int
 binarySearchHelper list target lower higher
-  | lower > higher = -1 -- if the lower index value is greater than higher it is a negative case
-  | target == middleValue = middle -- if the target value is equal to middleValue it is a positive case
-  | target < middleValue = binarySearchHelper list target lower (middle - 1) -- if the target value is less than the middleVlue find the target in the left part of the list
-  | otherwise = binarySearchHelper list target (middle + 1) higher -- if the target value is greater than the middleValue find the target value in the right part of the list
-  where
-    middle = (lower + higher) `div` 2 -- compute middle index 
-    middleValue = list !! middle      -- compute middle value
+ | lower > higher = -1
+ | target == middle_value = middle
+ | target > middle_value = binarySearchHelper list target (middle+1) higher
+ | otherwise = binarySearchHelper list target lower (middle-1)
+ where 
+  middle = (lower+higher) `div` 2
+  middle_value = list !! middle
 
 main :: IO ()
 main = do
-  let sortedList = [1,2..10]
-  print $ binarySearch sortedList (-1) -- negative case
-  print $ binarySearch sortedList 0    -- negative case
-  print $ binarySearch sortedList 5    -- positive case
-  print $ binarySearch sortedList 11   -- negative case
+ let sortedList = [1,2..10]
+ print$binarySearch sortedList 5
+ print$binarySearch sortedList (-1)
+ print$binarySearch sortedList 0
+ print$binarySearch sortedList 11
