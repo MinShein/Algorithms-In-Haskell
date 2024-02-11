db4372cd408a9c154017cf316fe2389ee1f8798d
MinShein <minthutakyae@gmail.com>
2024-02-11 13:07:37 +0630

    Haskell Algorithms

--- 0.1/Sort/Quicksort-0.1.hs
+++ 0.1/Sort/Quicksort-0.1.hs
@@ -4,5 +4,5 @@ quickSort [x,y] = (min x y):(max x y):[]
 quickSort (x:xs) = quickSort lower ++ pivot ++ quickSort higher
  where
      pivot = [x]
-     lower = [a| a <- xs, a <= x]
-     higher = [a| a <- xs, a > x]
+     lower = [low| low <- xs, low <= x]
+     higher = [high| high <- xs, high > x]
