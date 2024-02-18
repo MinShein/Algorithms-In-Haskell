f2f9cbe69f9b840b4bc6f03a70148e3cc5507cd8
MinShein <minthutakyae@gmail.com>
2024-02-19 01:12:37 +0630

    very basic Shunting Yard Algorithm

--- Infix-To-Postfix-Draft.hs
+++ Infix-To-Postfix-Draft.hs
@@ -1,37 +1,69 @@
+push :: Char -> [Char] -> [Char]
 push x [] = [x]
-push x xs = (x:xs)
+push x xs = reverse (x:(reverse xs))
 
+pop :: [Char] -> [Char]
 pop [] = []
-pop copy = popping copy [] []
- where
- popping [] opnd optr = (reverse opnd) ++ optr
- popping (x:xs) opnd optr
-  | elem x "()+-*/" = popping xs opnd (x:optr)
-  | otherwise = popping xs (x:opnd) optr
+pop (x:xs) = xs
 
-pop' [] = []
-pop' (_:xs) = xs
+peek :: [Char] -> Char
+peek (x:_) = x
 
-pop'' [] = []
-pop'' (x:xs)
- | not (elem x "()") = x:(pop'' xs)
- | otherwise = pop'' xs 
- 
+isEmpty :: [Char] -> Bool
 isEmpty [] = True
 isEmpty _  = False
 
+delimit :: [Char] -> Bool
 delimit [] = True
 delimit copy = matching copy []
  where
+ matching :: [Char] -> [Char] -> Bool
  matching [] ret = isEmpty ret
  matching (x:xs) ret
-  | elem x ")" = matching xs (push x ret)
-  | elem x "(" = matching xs (pop' ret)
+  | elem x "(" = matching xs (push x ret)
+  | elem x ")" = matching xs (pop ret)
   | otherwise = matching xs ret
-  
+
+isParenthesis :: Char -> Bool
+isParenthesis chr = elem chr "()"
+isOperand :: Char -> Bool
+isOperand chr = elem chr ['A'..'Z'] || elem chr ['a'..'z']
+isOperator :: Char -> Bool
+isOperator chr = elem chr "+-*/^"
+
+infixToPostfix :: [Char] -> [Char]
+infixToPostfix expr = helper expr [] []
+  where
+    helper :: [Char] -> [Char] -> [Char] -> [Char]
+    helper [] operand operator = operand ++ operator
+    helper copy@(x:xs) operand operator
+      | delimit copy == False = error "Delimiter not matched."
+      | isParenthesis x = helper xs operand operator
+      | isOperand x = helper xs (push x operand) operator
+      | isOperator x && isEmpty operator = helper xs operand (x:operator)
+      | isOperator x && not (isEmpty operator) =
+          if precedence x == precedence (peek operator)
+          then helper xs (push (peek operator) operand) (x:pop operator)
+          else if precedence x < precedence (peek operator)
+          then extractAll xs operand operator x
+          else helper xs operand (x:operator)
+      | otherwise = helper xs operand operator
+    precedence :: Char -> Int
+    precedence chr = case chr of
+      '+' -> 1
+      '-' -> 1
+      '*' -> 2
+      '/' -> 2
+      '^' -> 3
+      _   -> 0
+    extractAll :: [Char] -> [Char] -> [Char] -> Char -> [Char]
+    extractAll xs operand operator x = extractAll' xs operand operator x (length operator)
+      where
+        extractAll' :: [Char] -> [Char] -> [Char] -> Char -> Int -> [Char]
+        extractAll' xs operand [] x 0 = helper xs operand [x]
+        extractAll' xs operand (y:ys) x len = extractAll' xs (push y operand) ys x (len-1)
+
+main :: IO ()
 main = do
- let expr = "A*(B+C)-D"
- print $ expr
- print $ pop expr
- print $ pop'' (pop expr)
- print $ delimit (pop expr)
+ let expr = "A+B-C*D+E/F*E+G/A"
+ print $ infixToPostfix expr
