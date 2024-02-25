6a5a91e53eb3bb0ada22e0ea1f7aaa3e90f9a7fd
Min Thuta Shein <minthutakyae@gmail.com>
2024-02-25 21:14:27 +0630

    Update Infix-To-Postfix-Draft.hs

--- Infix-To-Postfix-Draft.hs
+++ Infix-To-Postfix-Draft.hs
@@ -1,5 +1,3 @@
-import Data.List
-
 p '+' = 1
 p '-' = 1
 p '*' = 2
@@ -7,13 +5,14 @@ p '/' = 2
 p '^' = 3
 p _   = 0
 
-tokenizer [] postfix stack
- = reverse (postfix) ++ (head stack):[]
+tokenizer [] postfix stack = reverse (postfix) ++ stack
 tokenizer (x:xs) postfix stack
+ | not (null stack) && (head stack) == '^'
+ = tokenizer xs ((reverse(take (length stack) stack))++postfix) (drop (length stack) stack)
  | x `elem` "("
  = tokenizer xs postfix (x:stack)
  | x `elem` ")"
- = tokenizer xs ((takeWhile (/= '(') stack)++postfix) (tail (dropWhile (== '(') stack))
+ = tokenizer xs ((takeWhile (/= '(') stack)++postfix) (tail (dropWhile (/= '(') stack))
  | x `elem` "+-*/^" && null stack
  = tokenizer xs postfix (x:stack)
  | x `elem` ['a'..'z']
@@ -22,11 +21,11 @@ tokenizer (x:xs) postfix stack
  = if p x > p (head stack)
    then tokenizer xs postfix (x:stack)
    else if p x <= p (head stack)
-   then tokenizer xs ((head stack):postfix) (x:delete (head stack)  stack)
+   then tokenizer xs ((head stack):postfix) (x:tail stack)
    else tokenizer xs postfix stack
  | otherwise 
  = tokenizer xs postfix stack
- 
+
 main = do
  print "Infix to Postfix"
- print $ tokenizer "((((a+b)-c)^d)/e)-f" [] []
+ print $ tokenizer "a*b+c/d^e-f*g" [] []
